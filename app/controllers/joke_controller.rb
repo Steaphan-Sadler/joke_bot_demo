5b2b49124bef9db0da05c7d71bc8bab09d8a516f
StoveSolder <ssadler@instructure.com>
2022-04-26 15:35:47 -0600

    update to messages following testing

--- app/controllers/joke_controller.rb
+++ app/controllers/joke_controller.rb
@@ -4,19 +4,19 @@ class JokeController < ApplicationController
   def fetch
     #get contact_id from webhook
     Rails.logger.debug params.inspect
-    @data = params[:data]
-    @contact_id = @data['endUserId']
+    message_id = params['data']['id']
+    
 
       #get the joke
-      fetched_joke = httparty.get("https://icanhazdadjoke.com/", {
-        headers: { "Accept" => "application/json" },
+      fetched_joke = HTTParty.get("https://icanhazdadjoke.com/", {
+        headers: { "Accept" => "application/json" }
       })
 
-    send_joke_headers = {"Authorization" => "Bearer #{ENV['JOKE_BOT_TOKEN']}", "Accept" => "application/json"}
-    send_joke_body = {"body" => {"attributes" => {"jokebot_joke" => "#{fetched_joke['joke']}"}}}
+    send_joke_headers = {"Authorization" => "Bearer #{ENV['JOKE_BOT_TOKEN']}", "content-type" => "application/json"}
+    send_joke_body = {"type" => "chat", "body" => fetched_joke['joke']}
 
       #send the joke to the conversation
-    send_joke_response = httparty.patch("https://driftapi.com/contacts/#{@contact_id}", headers: send_joke_headers, body: send_joke_body)
+    send_joke_response = HTTParty.post("https://driftapi.com/conversations/#{message_id}/messages", headers: send_joke_headers, body: send_joke_body)
     Rails.logger.debug "This was the response: #{send_joke_response}"
   end
 end
