36230400253eee6069ace93dbc3165d3dd8faf98
StoveSolder <ssadler@instructure.com>
2022-04-26 13:07:20 -0600

    updating controller with attributes and targeting contact model

--- app/controllers/joke_controller.rb
+++ app/controllers/joke_controller.rb
@@ -2,22 +2,20 @@ class JokeController < ApplicationController
   require "httparty"
 
   def fetch
-    #check webhook post for yes-ness
+    #get contact_id from webhook
     Rails.logger.debug params.inspect
-    if params[:data]['body'] == "Yes"
-      @convo_id = params[:orgId]
+      @contact_id = params['endUserId']
 
       #get the joke
       fetched_joke = httparty.get("https://icanhazdadjoke.com/", {
         headers: { "Accept" => "application/json" },
       })
 
+    send_joke_headers = {"Authorization" => "Bearer #{ENV['JOKE_BOT_TOKEN']}", "Accept" => "application/json"}
+    send_joke_body = {"body" => {"attributes" => {"jokebot_joke" = "#{fetched_joke['joke']}"}}}
+
       #send the joke to the conversation
-      httparty.post("https://driftapi.com/conversations/#{@convo_id}/messages", {
-        headers: { "Authorization" => "Bearer #{ENV["JOKE_BOT_TOKEN"]}" },
-        body: { "type" => "chat",
-                "body": fetched_joke["joke"] },
-      })
-    end
+    httparty.patch("https://driftapi.com/contacts/#{@contact_id}", headers: send_joke_headers, body: send_joke_body)
+
   end
 end
