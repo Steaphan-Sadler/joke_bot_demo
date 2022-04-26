e8436bf13023230fcdeb0bf28755a3bc25c4943e
StoveSolder <ssadler@instructure.com>
2022-04-26 13:12:14 -0600

    updating controller with attributes and targeting contact model

--- app/controllers/joke_controller.rb
+++ app/controllers/joke_controller.rb
@@ -12,10 +12,9 @@ class JokeController < ApplicationController
       })
 
     send_joke_headers = {"Authorization" => "Bearer #{ENV['JOKE_BOT_TOKEN']}", "Accept" => "application/json"}
-    send_joke_body = {"body" => {"attributes" => {"jokebot_joke" = "#{fetched_joke['joke']}"}}}
+    send_joke_body = {"body" => {"attributes" => {"jokebot_joke" => "#{fetched_joke['joke']}"}}}
 
       #send the joke to the conversation
     httparty.patch("https://driftapi.com/contacts/#{@contact_id}", headers: send_joke_headers, body: send_joke_body)
-
   end
 end
