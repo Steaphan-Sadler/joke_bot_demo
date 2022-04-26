92299d192ce0b83de305bfa90ec847ef808c8fa0
StoveSolder <ssadler@instructure.com>
2022-04-26 12:16:30 -0600

    fixing end-of-input on jokeController

--- app/controllers/joke_controller.rb
+++ app/controllers/joke_controller.rb
@@ -2,21 +2,21 @@ class JokeController < ApplicationController
   require "httparty"
 
   def fetch
-
+    #check webhook post for yes-ness
     if params[:data][:body] == "Yes"
-    @convo_id = params[:orgId]
+      @convo_id = params[:orgId]
 
-    #get the joke
-    fetched_joke = httparty.get("https://icanhazdadjoke.com/", {
-      headers: { "Accept" => "application/json" },
-    })
-    
-    #send the joke to the conversation
-    httparty.post("https://driftapi.com/conversations/#{@convo_id}/messages",{
-      headers: {"Authorization" => "Bearer: #{ENV['JOKE_BOT_TOKEN']}"},
-      body: {"type" =>"chat",
-      "body": fetched_joke['joke']}
-    })
+      #get the joke
+      fetched_joke = httparty.get("https://icanhazdadjoke.com/", {
+        headers: { "Accept" => "application/json" },
+      })
 
+      #send the joke to the conversation
+      httparty.post("https://driftapi.com/conversations/#{@convo_id}/messages", {
+        headers: { "Authorization" => "Bearer: #{ENV["JOKE_BOT_TOKEN"]}" },
+        body: { "type" => "chat",
+                "body": fetched_joke["joke"] },
+      })
+    end
   end
 end
