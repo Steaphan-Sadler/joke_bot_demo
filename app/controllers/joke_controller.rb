57bf773bab8f4a081ba95a78359fecbeeaf6a975
StoveSolder <ssadler@instructure.com>
2022-04-26 20:53:15 -0600

    further debugging

--- app/controllers/joke_controller.rb
+++ app/controllers/joke_controller.rb
@@ -5,15 +5,16 @@ class JokeController < ApplicationController
     #get contact_id from webhook
     Rails.logger.debug params.inspect
     message_id = params[:conversationId]
+    
       #get the joke
       fetched_joke = HTTParty.get("https://icanhazdadjoke.com/", {
         headers: { "Accept" => "application/json" }
       })
 
-    send_joke_headers = {"Authorization" => "Bearer #{ENV.fetch("JOKE_BOT_TOKEN")}", "content-type" => "application/json"}
+      Rails.logger.debug send_joke_headers = {"Authorization" => "Bearer #{ENV.fetch("JOKE_BOT_TOKEN")}", "content-type" => "application/json"}
     send_joke_body = {"type" => "chat", "body" => fetched_joke['joke']}
 
       #send the joke to the conversation
-      Rails.logger.debug "What we're sending is #{HTTParty.post("https://driftapi.com/conversations/#{message_id}/messages", headers: send_joke_headers, body: send_joke_body.to_json)}"
+      HTTParty.post("https://driftapi.com/conversations/#{message_id}/messages", headers: send_joke_headers, body: send_joke_body.to_json)}
   end
 end
