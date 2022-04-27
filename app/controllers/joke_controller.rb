7b1fc53b892aa134a7e262d5f744432e0cdf4ae2
StoveSolder <ssadler@instructure.com>
2022-04-27 00:17:34 -0600

    just some cleanup while preparring walk through

--- app/controllers/joke_controller.rb
+++ app/controllers/joke_controller.rb
@@ -2,17 +2,16 @@ class JokeController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def fetch
-    message_body = params[:data][:body]
-    contact_id = params[:data][:author][:id] 
     #get the joke we'll send somewhere
     fetched_joke = HTTParty.get("https://icanhazdadjoke.com/", {
       headers: { "Accept" => "application/json" },
     })
 
+    #prep headers for Drift API
     send_joke_headers = { "Authorization" => "Bearer #{ENV.fetch("JOKE_BOT_TOKEN")}", "content-type" => "application/json" }
 
     #if user answers yes, we'll send a joke
-    if message_body == "Yes"
+    if params[:data][:body] == "Yes"
       # message_id = params[:data][:conversationId]
       # send_joke_body = { "type" => "chat", "body" => fetched_joke["joke"] }
       # #send the joke to the conversation
@@ -20,7 +19,7 @@ class JokeController < ApplicationController
 
       send_joke_body = { "attributes" => { "jokebot_joke" => fetched_joke["joke"] } }
       #for updating contact modle with joke
-      HTTParty.patch("https://driftapi.com/contacts/#{contact_id}", headers: send_joke_headers, body: send_joke_body.to_json)
+      HTTParty.patch("https://driftapi.com/contacts/#{params[:data][:author][:id]}", headers: send_joke_headers, body: send_joke_body.to_json)
     end
   end
 end
